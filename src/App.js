de1abdb7925987f11be78a343cf785a267803815
Kimberly Encinas <kimbencinas@gmail.com>
2024-04-18 23:30:26 -0700

    setup react for my project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>NoDairyDelish</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
