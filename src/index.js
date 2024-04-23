10495c4675eacfb6e996c52d54f288c7056f21ea
Kimberly Encinas <kimbencinas@gmail.com>
2024-04-22 23:13:39 -0700

    set up react router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
-import './App.css';
+import Meals from './Pages/Meals';
+import Ingredients from './Pages/Ingredients';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/meals',
+    element: <Meals />
+  },
+  {
+    path: '/ingredients',
+    element: <Ingredients />
+  },
+  {
+    path: '/about',
+    element: <About />
+  },
+  {
+    path: '/contact',
+    element: <Contact />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
