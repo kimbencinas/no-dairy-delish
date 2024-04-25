ec63cc8418ae28712b3f9d710b48a048cf7fb4cc
Kimberly Encinas <kimbencinas@gmail.com>
2024-04-24 17:32:00 -0700

    add react router links to each page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
 
   return (
     <div className="flex flex-row bg-gray-200 p-5 gap-8 justify-end">
         <h2>NoDairyDelish</h2>
-        <p>Meals</p>
-        <p>Ingredients</p>
-        <p>About Us</p>
-        <p>Contact Us</p>
+        <Link to="/meals">Meals</Link>
+        <Link to="/ingredients">Ingredients</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact Us</Link>
     </div>
   );
 }
