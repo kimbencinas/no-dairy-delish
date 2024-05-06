fbd5cdf61c6c9fa61caf8cd54d4025e511990878
Kimberly Encinas <kimbencinas@gmail.com>
2024-05-05 23:38:48 -0700

    Add vegan recipes to meals page

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,41 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 function Feed() {
-    const [feedList, setFeedList] = useState([]);
 
-    useEffect(() => {
-        const feedData = async () => {
-            const url = 'https://tasty.p.rapidapi.com/feeds/list?size=5&timezone=%2B0700&vegetarian=false&from=0';
-            const options = {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Key': '46bdcb908amsh37465532556edf5p168cafjsn62a83629af8d',
-                    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-                }
-            };
-
-            try {
-                const response = await fetch(url, options);
-                const result = await response.json();
-                setFeedList(result);
-            } catch(error) {
-                console.error(error);
-            }
-        };
-
-        feedData();
-    }, []);
-
-    return (
-    <div>
-        <h2>Feed List</h2>
-        <ul>
-            {feedList.map((item, index) => (
-                <li key={index}>{item.title}</li>
-            ))}
-        </ul>
-    </div>
-  );
 }
 
 export default Feed;
--- src/Pages/Meals.jsx
+++ src/Pages/Meals.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar';
 
 function Meals() {
+  const [feedList, setFeedList] = useState([]);
+
+  useEffect(() => {
+      const feedData = async () => {
+          const url = 'https://the-vegan-recipes-db.p.rapidapi.com/';
+          const options = {
+              method: 'GET',
+              headers: {
+                  'X-RapidAPI-Key': '46bdcb908amsh37465532556edf5p168cafjsn62a83629af8d',
+                  'X-RapidAPI-Host': 'the-vegan-recipes-db.p.rapidapi.com'
+              }
+          };
+  
+          try {
+              const response = await fetch(url, options);
+              if(!response.ok) {
+                  throw new Error('Response was not ok.');
+              }
+              const result = await response.json();
+              console.log(result);
+              setFeedList(result);
+          } catch(error) {
+              console.error(error);
+          }
+      };
+  
+      feedData();
+  }, []);
+  
 
   return (
     <div>
       <Navbar />
+      <div>
+        <h2>Vegan Recipes</h2>
+        {feedList.map((recipe, index) => (
+          <div key={index}>
+            <img src={recipe.image} alt={recipe.title} />
+            <p>{recipe.title}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
