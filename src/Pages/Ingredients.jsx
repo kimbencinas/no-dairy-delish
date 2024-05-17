d37d0783225280a853e8549795691b235e05acee
Kimberly Encinas <kimbencinas@gmail.com>
2024-05-16 23:56:22 -0700

    Update API url, user able to see recipes based on ingredient

--- src/Pages/Ingredients.jsx
+++ src/Pages/Ingredients.jsx
@@ -7,7 +7,7 @@ function Ingredients() {
 
   useEffect(() => {
     const getItemPrice = async () => {
-      const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/1003464/priceBreakdownWidget.json';
+      const url = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=apple&number=1&ignorePantry=true&ranking=1';
 
       const options = {
         method: 'GET',
@@ -19,14 +19,18 @@ function Ingredients() {
 
       try {
         const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error('Error');
+        }
         const result = await response.json();
         console.log(result);
+        setItemPrice(result.itemPrice);
       } catch(error) {
         console.error(error);
       }
-
-    }
-  })
+    };
+    getItemPrice();
+  }, []);
 
   return (
     <div>
