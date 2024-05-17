fa1243205b92b7eae382b0ec1b770325e3d69862
Kimberly Encinas <kimbencinas@gmail.com>
2024-05-17 13:46:35 -0700

    Add form to contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Components/Navbar';
 
 function Contact() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
   return (
     <div>
       <Navbar />
+      <div>
+        <h2>Contact Us</h2>
+        <div className="flex flex-col">
+          <form>
+            <div className="">
+              <label>FIRST NAME
+                <input
+                  type="text"
+                  value={firstName}
+                />
+              </label>
+              <label>LAST NAME
+                <input
+                  type="text"
+                  value={lastName}
+                />
+              </label>
+            </div>
+            <label>EMAIL
+              <input  
+                type="text"
+                value={email}
+              />
+            </label>
+            <label>MESSAGE
+              <textarea
+                value={message}
+              />
+            </label>
+            <input type="submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
