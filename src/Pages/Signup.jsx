65f8b4c10d8372b26590ddb1246b95b82b5b1a27
Kimberly Encinas <kimbencinas@gmail.com>
2024-05-21 23:58:34 -0700

    Added some styling changes to signup page

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -4,49 +4,49 @@ import Navbar from '../Components/Navbar';
 function Signup() {
 
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-200 h-screen">
       <Navbar />
-      <div className="main-container flex items-center justify-center">
-        <div className="bg-white p-4 rounded-xl shadow-md">
-            <h2 className="text-center">Sign Up</h2>
+      <div className="main-container flex justify-center">
+        <div className="bg-white p-4 rounded-xl shadow-md mt-60 px-10 py-14">
+            <h2 className="text-center text-2xl font-semibold mb-5">Sign Up</h2>
             <form>
                 <div className="flex">
-                    <div className="mb-2 p-4">
+                    <div className="mb-1 p-3">
                         <label className="text-black block">
                             Name
                         </label>
                         <input
                             type="text"
-                            className="p-2 border"
+                            className="p-2 border rounded-md"
                         />
                     </div>
-                    <div className="mb-2 p-4">
+                    <div className="mb-1 p-3">
                         <label className="text-black block">
                             Username
                         </label>
                         <input
                             type="text"
-                            className="p-2 border"
+                            className="p-2 border rounded-md"
                         />
                     </div>
                 </div>
                 <div className="flex">
-                    <div className="mb-2 p-4">
+                    <div className="mb-4 p-3">
                         <label className="text-black block">
                                 Password
                         </label>
                         <input
                             type="password"
-                            className="p-2 border"
+                            className="p-2 border rounded-md"
                         />
                     </div>
-                    <div className="mb-2 p-4">
+                    <div className="mb-4 p-3">
                         <label className="text-black block">
                                 Confirm Password
                         </label>
                         <input
                             type="password"
-                            className="p-2 border"
+                            className="p-2 border rounded-md w-full"
                         />
                     </div>
                 </div>
