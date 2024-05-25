edb3be8d47f207c1c8d622845a377cd75d472fee
Kimberly Encinas <kimbencinas@gmail.com>
2024-05-25 10:42:04 -0700

    Created dropdown menu for meals section on navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
   <div className="flex flex-row bg-emerald-200 p-3 justify-center gap-80">
       <div className="flex gap-2">
@@ -15,12 +19,16 @@ function Navbar() {
         />
       </div>
       <div className="flex flex-row gap-5 items-end mb-1">
-        <Link to="/meals">Meals</Link>
-        <ul>
-          <DropdownItem text="Breakfast" to="/breakfast"></DropdownItem>
-          <DropdownItem text="Lunch" to="/lunch"></DropdownItem>
-          <DropdownItem text="Dinner" to="/dinner"></DropdownItem>
+        <button onClick={toggleDropdown} className="">
+          Meals
+        </button>
+        {isOpen && (
+          <ul className="absolute top-10 py-3 px-6 bg-gray-100">
+            <DropdownItem text="Breakfast" to="/breakfast"></DropdownItem>
+            <DropdownItem text="Lunch" to="/lunch"></DropdownItem>
+            <DropdownItem text="Dinner" to="/dinner"></DropdownItem>
         </ul>
+        )}
         <Link to="/ingredients">Ingredients</Link>
         <Link to="/about">About Us</Link>
         <Link to="/contact">Contact Us</Link>
