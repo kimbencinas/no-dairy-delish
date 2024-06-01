2fcb6410a29606803cbbaf28855685505d3f4279
Kimberly Encinas <kimbencinas@gmail.com>
2024-05-31 17:55:36 -0700

    Add formik to contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Navbar from '../Components/Navbar';
+import { useFormik } from 'formik';
 
 function Contact() {
   /* const [firstName, setFirstName] = useState('');
@@ -7,6 +8,17 @@ function Contact() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 */
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      firstName: '',
+      lastName: '',
+      message: '',
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null))
+    },
+  });
 
   return (
     <div className="bg-white">
@@ -14,35 +26,51 @@ function Contact() {
       <h2 className="text-center p-3 mt-10 mb-3">Contact Us</h2>
       <div className="main-container flex items-center justify-center">
         <div className="bg-gray-100 p-7 rounded-xl shadow-md">
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <div className="flex">
               <div className="mb-4 p-2">
                 <label className="block text-black">FIRST NAME</label>
                   <input
+                    id="firstName"
+                    name="firstName"
                     type="text"
                     className="shadow border rounded p-1"
+                    value={formik.values.firstName}
+                    onChange={formik.handleChange}
                   />
                 
               </div>
               <div className="mb-4 p-2">
                 <label className="block">LAST NAME</label>
                   <input
+                    id="lastName"
+                    name="lastName"
                     type="text"
                     className="shadow border rounded p-1"
+                    value={formik.values.lastName}
+                    onChange={formik.handleChange}
                   />
               </div>
             </div>
             <div className="mb-4 p-2">
               <label className="block">EMAIL</label>
                 <input  
+                  id="email"
+                  name="email"
                   type="text"
                   className="shadow border rounded w-full p-1"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
                 />
             </div>
             <div className="mb-4 p-2">
               <label className="block">MESSAGE</label>
                 <textarea
+                  id="message"
+                  name="message"
                   className="shadow border rounded w-full p-1"
+                  value={formik.values.message}
+                  onChange={formik.handleChange}
                 />
             </div>
             <div>
