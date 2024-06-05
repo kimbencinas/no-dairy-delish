1f6f07500483cff5cf6e9dab0a84f793cd7c9f5a
Kimberly Encinas <kimbencinas@gmail.com>
2024-06-04 23:49:31 -0700

    Fixed error with api search recipe method

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,43 +2,48 @@
 
     function Search() {
         const [recipe, setRecipe] = useState([]);
+        const [query, setQuery] = useState('');
 
-        useEffect(() => {
-            const searchRecipes = async () => {
-                const url = 'https://api.spoonacular.com/recipes/complexSearch';
-                const options = {
-                    method: 'GET',
-                    headers: {
-                    'X-RapidAPI-Key': '46bdcb908amsh37465532556edf5p168cafjsn62a83629af8d',
-                    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'  
-                    }
-                };
-
-                try {
-                    const response = await fetch(url, options);
-                    if (!response.ok) {
-                        throw new Error("Error");
-                    }
-                    const result = await response.json();
-                    console.log(result);
-                    setRecipe(result.recipes);
-                    
-                } catch (error) {
-                    console.error(error);
+        const searchRecipes = async () => {
+            const url = `https://api.spoonacular.com/recipes/complexSearch?query=${query}`;
+            const options = {
+                method: 'GET',
+                headers: {
+                'X-RapidAPI-Key': '46bdcb908amsh37465532556edf5p168cafjsn62a83629af8d',
+                'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'  
                 }
             };
+
+            try {
+                const response = await fetch(url, options);
+                if (!response.ok) {
+                    throw new Error("Error");
+                }
+                const result = await response.json();
+                console.log(result);
+                setRecipe(result.results);
+                    
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        const handleChange = (e) => {
+            setQuery(e.target.value);
+        };
+        const handleSearch = (e) => {
+            e.preventDefault();
             searchRecipes();
-        }, []);
+        };
 
-        return (
-            <div>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="rounded-xl p-1 focus:outline-none focus:ring focus-border-purple-500 w-72"
-                />
-            </div>
-        )
-    }
+    return (
+        <div>
+            <input
+                type="text"
+                placeholder="Search..."
+                className="rounded-xl p-1 focus:outline-none focus:ring focus-border-purple-500 w-72"
+            />
+        </div>
+    )
+}
 
-    export default Search;
+export default Search;
