33655d48a9feabd9dbb9d75f3afb7d42af8de314
Kimberly Encinas <kimbencinas@gmail.com>
2024-06-20 23:57:38 -0700

    Add content to lunch & dinner page

--- src/Pages/Lunch.jsx
+++ src/Pages/Lunch.jsx
@@ -6,6 +6,12 @@ function Lunch() {
   return (
     <div>
       <Navbar />
+      <div className="container mx-auto">
+      <h2 className="font-semibold text-center text-2xl mt-10">Lunch</h2>
+        <div className="flex flex-wrap justify-center items-center gap-10 p-4">
+           
+        </div>
+      </div>
     </div>
   );
 }
