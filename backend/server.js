417204f05f0ea2b04483ce69f35f8cc1e322ebcb
Kimberly Encinas <kimbencinas@gmail.com>
2025-01-29 18:15:22 -0800

    Worked on signup functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bcrypt = require('bcrypt');
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
@@ -17,4 +18,29 @@ const pool = new Pool({
 
 app.listen(5000, () => {
     console.log("Server has started on port 5000");
+});
+
+app.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const userExists = await pool.query(
+        'SELECT * FROM users WHERE username = $1 OR email = $2',
+        [username, email]
+    );
+    if (userExists.rows.length > 0) {
+        return res.status(400).json({ message: 'User already exists' });
+    }
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await pool.query(
+            'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [username, email, hashedPassword]
+        );
+        res.status(201).json({ message: 'User created successfully', user: newUser.rows[0]});
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
